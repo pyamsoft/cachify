116add17b30874950577ac63fd27e4b6e2705aaf
pyamsoft <developer.pyamsoft@gmail.com>
2023-03-16 16:11:28 -0700

    Deprecate multi argument cachify as they are unusual

--- cachify/src/main/java/com/pyamsoft/cachify/SingleEntry.kt
+++ cachify/src/main/java/com/pyamsoft/cachify/SingleEntry.kt
@@ -58,6 +58,22 @@ public inline fun <R : Any> cachify(
 /** Wrapper which will generate a Cached object that delegates its call() to the upstream source */
 @CheckResult
 @JvmOverloads
+@Deprecated("""Unexpected API behavior
+
+The cachify{} construct with parameters had an unexpected implementation.
+When parameters changed, the upstream would not be fetched with the new parameters if
+a previous result was still valid in the cache - rather that old stale data using old parameters
+was returned to the caller.
+
+While potentially useful in some very niche cases, this behavior was largely unexpected.
+
+Users hoping for a more regular behavior are encouraged to migrate over to multiCachify{}
+calls. The multiCachify{} construct takes a Key parameter and handles a unique cache instance
+per unique key. As long as parameters that construct the Key are deterministic and unchanged,
+the same result will continue to be cached and returned. Changes to parameters which construct
+the Key return different results as one would expect.
+""",
+)
 public inline fun <R : Any, T1> cachify(
     context: CoroutineContext = CachifyDefaults.DEFAULT_COROUTINE_CONTEXT,
     debugTag: String = "",
@@ -81,6 +97,22 @@ public inline fun <R : Any, T1> cachify(
 /** Wrapper which will generate a Cached object that delegates its call() to the upstream source */
 @CheckResult
 @JvmOverloads
+@Deprecated("""Unexpected API behavior
+
+The cachify{} construct with parameters had an unexpected implementation.
+When parameters changed, the upstream would not be fetched with the new parameters if
+a previous result was still valid in the cache - rather that old stale data using old parameters
+was returned to the caller.
+
+While potentially useful in some very niche cases, this behavior was largely unexpected.
+
+Users hoping for a more regular behavior are encouraged to migrate over to multiCachify{}
+calls. The multiCachify{} construct takes a Key parameter and handles a unique cache instance
+per unique key. As long as parameters that construct the Key are deterministic and unchanged,
+the same result will continue to be cached and returned. Changes to parameters which construct
+the Key return different results as one would expect.
+""",
+)
 public inline fun <R : Any, T1, T2> cachify(
     context: CoroutineContext = CachifyDefaults.DEFAULT_COROUTINE_CONTEXT,
     debugTag: String = "",
@@ -107,6 +139,22 @@ public inline fun <R : Any, T1, T2> cachify(
 /** Wrapper which will generate a Cached object that delegates its call() to the upstream source */
 @CheckResult
 @JvmOverloads
+@Deprecated("""Unexpected API behavior
+
+The cachify{} construct with parameters had an unexpected implementation.
+When parameters changed, the upstream would not be fetched with the new parameters if
+a previous result was still valid in the cache - rather that old stale data using old parameters
+was returned to the caller.
+
+While potentially useful in some very niche cases, this behavior was largely unexpected.
+
+Users hoping for a more regular behavior are encouraged to migrate over to multiCachify{}
+calls. The multiCachify{} construct takes a Key parameter and handles a unique cache instance
+per unique key. As long as parameters that construct the Key are deterministic and unchanged,
+the same result will continue to be cached and returned. Changes to parameters which construct
+the Key return different results as one would expect.
+""",
+)
 public inline fun <R : Any, T1, T2, T3> cachify(
     context: CoroutineContext = CachifyDefaults.DEFAULT_COROUTINE_CONTEXT,
     debugTag: String = "",
@@ -134,6 +182,22 @@ public inline fun <R : Any, T1, T2, T3> cachify(
 /** Wrapper which will generate a Cached object that delegates its call() to the upstream source */
 @CheckResult
 @JvmOverloads
+@Deprecated("""Unexpected API behavior
+
+The cachify{} construct with parameters had an unexpected implementation.
+When parameters changed, the upstream would not be fetched with the new parameters if
+a previous result was still valid in the cache - rather that old stale data using old parameters
+was returned to the caller.
+
+While potentially useful in some very niche cases, this behavior was largely unexpected.
+
+Users hoping for a more regular behavior are encouraged to migrate over to multiCachify{}
+calls. The multiCachify{} construct takes a Key parameter and handles a unique cache instance
+per unique key. As long as parameters that construct the Key are deterministic and unchanged,
+the same result will continue to be cached and returned. Changes to parameters which construct
+the Key return different results as one would expect.
+""",
+)
 public inline fun <R : Any, T1, T2, T3, T4> cachify(
     context: CoroutineContext = CachifyDefaults.DEFAULT_COROUTINE_CONTEXT,
     debugTag: String = "",
@@ -162,6 +226,22 @@ public inline fun <R : Any, T1, T2, T3, T4> cachify(
 /** Wrapper which will generate a Cached object that delegates its call() to the upstream source */
 @CheckResult
 @JvmOverloads
+@Deprecated("""Unexpected API behavior
+
+The cachify{} construct with parameters had an unexpected implementation.
+When parameters changed, the upstream would not be fetched with the new parameters if
+a previous result was still valid in the cache - rather that old stale data using old parameters
+was returned to the caller.
+
+While potentially useful in some very niche cases, this behavior was largely unexpected.
+
+Users hoping for a more regular behavior are encouraged to migrate over to multiCachify{}
+calls. The multiCachify{} construct takes a Key parameter and handles a unique cache instance
+per unique key. As long as parameters that construct the Key are deterministic and unchanged,
+the same result will continue to be cached and returned. Changes to parameters which construct
+the Key return different results as one would expect.
+""",
+)
 public inline fun <R : Any, T1, T2, T3, T4, T5> cachify(
     context: CoroutineContext = CachifyDefaults.DEFAULT_COROUTINE_CONTEXT,
     debugTag: String = "",
