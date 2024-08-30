5c6e03424b7a9a5a7774bfb4e49d5ed77bc1df73
pyamsoft <developer.pyamsoft@gmail.com>
2024-08-29 22:59:25 -0700

    Bump desugar

--- cachify/build.gradle.kts
+++ cachify/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 }
 
 dependencies {
-  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.0")
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.1")
 
   // AndroidX Annotations
   implementation("androidx.annotation:annotation:1.8.2")
