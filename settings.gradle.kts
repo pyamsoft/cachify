d69cc594f8be28b6b592ed968cf7c6504308d739
pyamsoft <developer.pyamsoft@gmail.com>
2026-08-26 07:01:18 -0700

    Move dep resolution into settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,21 @@
 
 pluginManagement {
   repositories {
+    mavenLocal()
     gradlePluginPortal()
+    mavenCentral()
+    google()
+  }
+}
+
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+  repositories {
+    mavenLocal()
     google()
     mavenCentral()
+    gradlePluginPortal()
   }
 }
 
